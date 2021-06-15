f7b60af907db8744871bb3d8172e376b1f97fa8f
saibibarbachane <60187657+saibibarbachane@users.noreply.github.com>
2021-06-15 16:30:37 +0200

    Add project files.

---
+++ CompanyRegister/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CompanyRegister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Workers workers = new Workers();
+            List<Workers> people = new List<Workers>();
+            bool check = true;
+            while (check)
+            {
+                Console.WriteLine("Please insert a Name or q to quit:");
+                //string input = Console.ReadLine(); 
+                workers.Name = Console.ReadLine();
+                if (workers.Name == "q")
+                {
+                    break;
+                    
+                }
+
+                Console.WriteLine("Please insert a Salary:");
+                try
+                {
+                    workers.Salary = double.Parse(Console.ReadLine());
+
+                }
+                catch (ArgumentException ex)
+                {
+                    workers.Salary = 0;
+                   
+                }
+
+
+            }
+            
+            foreach (var item in people)
+            {
+                Console.WriteLine($"Worker Name: {item.Name} , Worker Salary: {item.Salary}");
+            }
+
+
+
+        }
+
+    }
+}
---
+++ CompanyRegister/Workers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyRegister
+{
+    public class Workers
+    {
+        public string Name;
+        public double Salary;
+        
+    }
+}
